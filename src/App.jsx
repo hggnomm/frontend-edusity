d34af665b1c9d7613ca362c115d21440ebc12c31
hggnomm <hggnomm.dev@gmail.com>
2024-11-14 21:54:53 +0700

    Done title view

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,17 @@ import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
 import Programs from "./components/Programs/Programs";
+import Title from "./components/Title/Title";
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Hero />
-      {/* <div className="container"> */}
-      <Programs />
-      {/* </div> */}
+      <div className="container">
+        <Title subTitle="Our PROGRAM" title="What We Offer" />
+        <Programs />
+      </div>
     </div>
   );
 };
