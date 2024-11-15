4e202d4eb0525d4a84f6fa524ca69663328089c5
hggnomm <hggnomm.dev@gmail.com>
2024-11-15 22:17:26 +0700

    [!IMPORTANT] React Scroll page

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import "./Navbar.css";
 import logo from "../../assets/logo.png";
+import { Link } from "react-scroll";
 const Navbar = () => {
   const [sticky, setSticky] = useState(false);
 
@@ -13,13 +14,35 @@ const Navbar = () => {
     <nav className={`container ${sticky ? "dark-nav" : ""}`}>
       <img src={logo} alt="" className="logo" />
       <ul>
-        <li>Home</li>
-        <li>Program</li>
-        <li>About</li>
-        <li>Campus</li>
-        <li>Testimonials</li>
         <li>
-          <button className="btn">Contact us</button>
+          <Link to="hero" smooth={true} offset={0} duration={500}>
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to="programs" smooth={true} offset={-260} duration={500}>
+            Program
+          </Link>
+        </li>
+        <li>
+          <Link to="about" smooth={true} offset={-150} duration={500}>
+            About
+          </Link>
+        </li>
+        <li>
+          <Link to="campus" smooth={true} offset={-260} duration={500}>
+            Campus
+          </Link>
+        </li>
+        <li>
+          <Link to="testimonials" smooth={true} offset={-260} duration={500}>
+            Testimonials
+          </Link>
+        </li>
+        <li>
+          <Link to="contact" smooth={true} offset={-260} duration={500}>
+            <button className="btn">Contact us</button>
+          </Link>
         </li>
       </ul>
     </nav>
