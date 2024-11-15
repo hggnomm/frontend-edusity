4a7dd17904d2ba645abc84de19b694e2ec187ddd
hggnomm <hggnomm.dev@gmail.com>
2024-11-16 00:31:59 +0700

    Video player

---
+++ src/components/VideoPlayer/VideoPlayer.jsx
@@ -0,0 +1,22 @@
+import React, { useRef } from "react";
+import "./VideoPlayer.css";
+import video from "../../assets/videoplayback.mp4";
+const VideoPlayer = ({ playVideo, setPlayVideo }) => {
+  const player = useRef(null);
+  const closePlayer = (e) => {
+    if (e.target === player) {
+      setPlayVideo(false);
+    }
+  };
+  return (
+    <div
+      className={`video-player ${playVideo ? "" : "hide"}`}
+      ref={player}
+      onClick={closePlayer}
+    >
+      <video src={video} autoPlay muted controls></video>
+    </div>
+  );
+};
+
+export default VideoPlayer;
