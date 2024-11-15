8c6ebff46cc88448cbe810a82914ef182f05d303
hggnomm <hggnomm.dev@gmail.com>
2024-11-15 21:58:15 +0700

    *** [!IMPORTANT] api send mail with web3form

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -7,6 +7,31 @@ import location_icon from "../../assets/location-icon.png";
 import white_arrow from "../../assets/white-arrow.png";
 
 const Contact = () => {
+  const [result, setResult] = React.useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", import.meta.env.VITE_WEB3FORM_ACCESS_KEY);
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData,
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("Form Submitted Successfully");
+      setResult(res.message);
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      setResult(data.message);
+    }
+  };
   return (
     <div className="contact">
       <div className="contact-col">
@@ -35,7 +60,7 @@ const Contact = () => {
         </ul>
       </div>
       <div className="contact-col">
-        <form>
+        <form onSubmit={onSubmit}>
           <label>Your name</label>
           <input
             type="text"
@@ -61,7 +86,7 @@ const Contact = () => {
             Submit now <img src={white_arrow} alt="" />
           </button>
         </form>
-        <span></span>
+        <span>{result}</span>
       </div>
     </div>
   );
